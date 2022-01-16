bc13464eb43c419d02cefb27c80a5373353d77c4
Jeevan Cyriac <jeevantom@hotmail.com>
2022-01-16 18:35:34 +0000

    Adding subnets, nat-gws, routetbls to template. using cfn deploy cli command

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 TEMPLATE_FILE_NAME=webserver-cfn.yml
-STACK_NAME=jeevan-webserver
+STACK_NAME=jeevan-webservers
 AWS_REGION=us-east-1
 AWS_PROFILE=prod-apptest3
-STACK_ACTION=${1:-update}
 
-aws cloudformation ${STACK_ACTION}-stack \
+aws cloudformation deploy \
     --profile $AWS_PROFILE \
     --region $AWS_REGION \
-    --template-body file://$TEMPLATE_FILE_NAME \
+    --template-file $TEMPLATE_FILE_NAME \
     --stack-name $STACK_NAME
